a511f04ec6bd7abe91e4fc53cb44153a7e29e854
wyn <1224149409@qq.com>
2021-01-22 16:19:42 +0800

    项目构建

---
+++ src/main/java/com/janin/JaninApplication.java
@@ -0,0 +1,13 @@
+package com.janin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaninApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaninApplication.class, args);
+    }
+
+}
